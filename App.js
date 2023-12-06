0b04baad6a8f3ac22165caf6eb611f3244097cbc
Siv0k <artvlad2003@gmail.com>
2023-12-07 00:56:56 +0300

    end of developing

--- App.js
+++ App.js
@@ -1,18 +1,99 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Minesweeper from './src/Minesweeper';
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, Alert, TextInput} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Minesweeper from "./src/Minesweeper";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Minesweeper" component={Minesweeper} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Settings" component={SettingsScreen}/>
+                <Stack.Screen name="Minesweeper" component={Minesweeper}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
+
+const SettingsScreen = ({navigation}) => {
+    const [width, setWidth] = useState('5');
+    const [height, setHeight] = useState('5');
+
+
+    const isValidInput = (value) => {
+        return /^[1-9]\d*$/.test(value);
+    };
+
+    const handleStartGame = () => {
+        if (isValidInput(width) && isValidInput(height)
+            && parseInt(width, 10) <= 11 && parseInt(height, 10) <= 11
+            && parseInt(width, 10) >= 3 && parseInt(height, 10) >= 3) {
+            navigation.navigate('Minesweeper', {width: parseInt(width, 10), height: parseInt(height, 10)});
+        } else {
+            Alert.alert('Неправильный ввод', 'Введите целое число от 1 до 11');
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Настройки</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Высота: </Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="numeric"
+                    value={width}
+                    onChangeText={(text) => setWidth(text)}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Ширина: </Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="numeric"
+                    value={height}
+                    onChangeText={(text) => setHeight(text)}
+                />
+            </View>
+            <Button title="Начать" onPress={handleStartGame} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 16,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    inputLabel: {
+        fontSize: 16,
+        marginRight: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 8,
+        width: 50,
+    },
+});
 
 export default App;
--- src/Minesweeper.jsx
+++ src/Minesweeper.jsx
@@ -1,70 +1,79 @@
-import React, { useState } from 'react';
-import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Platform, Alert } from 'react-native';
 
-const Minesweeper = () => {
-	const rows = 5;
-	const cols = 5;
-	const minesCount = 5;
+function initializeBoard(width, height, minesCount) {
+	const board = Array.from({ length: width }, () =>
+		Array.from({ length: height }, () => ({
+			isMine: false,
+			isOpen: false,
+			isFlagged: false,
+			neighborMines: 0,
+		}))
+	);
 
-	const [board, setBoard] = useState(initializeBoard(rows, cols, minesCount));
+	let minesPlaced = 0;
+	while (minesPlaced < minesCount) {
+		const randomRow = Math.floor(Math.random() * width);
+		const randomCol = Math.floor(Math.random() * height);
 
-	function initializeBoard(rows, cols, minesCount) {
-		const board = Array.from({ length: rows }, () =>
-			Array.from({ length: cols }, () => ({
-				isMine: false,
-				isOpen: false,
-				isFlagged: false,
-				neighborMines: 0,
-			}))
-		);
-
-		let minesPlaced = 0;
-		while (minesPlaced < minesCount) {
-			const randomRow = Math.floor(Math.random() * rows);
-			const randomCol = Math.floor(Math.random() * cols);
-
-			if (!board[randomRow][randomCol].isMine) {
-				board[randomRow][randomCol].isMine = true;
-				minesPlaced++;
-			}
+		if (!board[randomRow][randomCol].isMine) {
+			board[randomRow][randomCol].isMine = true;
+			minesPlaced++;
 		}
-
-		for (let row = 0; row < rows; row++) {
-			for (let col = 0; col < cols; col++) {
-				if (!board[row][col].isMine) {
-					const neighbors = [
-						[-1, -1], [-1, 0], [-1, 1],
-						[0, -1],           [0, 1],
-						[1, -1], [1, 0], [1, 1],
-					];
-
-					let mineCount = 0;
-					neighbors.forEach(([dx, dy]) => {
-						const newRow = row + dx;
-						const newCol = col + dy;
-
-						if (
-							newRow >= 0 &&
-							newRow < rows &&
-							newCol >= 0 &&
-							newCol < cols &&
-							board[newRow][newCol].isMine
-						) {
-							mineCount++;
-						}
-					});
-
-					board[row][col].neighborMines = mineCount;
-				}
-			}
-		}
-
-		return board;
 	}
 
+	for (let row = 0; row < width; row++) {
+		for (let col = 0; col < height; col++) {
+			if (!board[row][col].isMine) {
+				const neighbors = [
+					[-1, -1], [-1, 0], [-1, 1],
+					[0, -1], [0, 1],
+					[1, -1], [1, 0], [1, 1],
+				];
+
+				let mineCount = 0;
+				neighbors.forEach(([dx, dy]) => {
+					const newRow = row + dx;
+					const newCol = col + dy;
+
+					if (
+						newRow >= 0 &&
+						newRow < width &&
+						newCol >= 0 &&
+						newCol < height &&
+						board[newRow][newCol].isMine
+					) {
+						mineCount++;
+					}
+				});
+
+				board[row][col].neighborMines = mineCount;
+			}
+		}
+	}
+
+	return board;
+}
+
+const Minesweeper = ({route}) => {
+		const { width, height } = route.params;
+		const minesCount = Math.ceil((width * height) * 0.15);
+
+		const [board, setBoard] = useState(initializeBoard(width, height, minesCount));
+		const [flagMode, setFlagMode] = useState(false);
+		const [gameOver, setGameOver] = useState(false);
+		const [win, setWin] = useState(false);
+
+		useEffect(() => {
+			checkWin();
+		}, [board]);
+
+	function toggleFlagMode() {
+		setFlagMode(!flagMode);
+	}
 
 	function revealCell(row, col) {
-		if (board[row][col].isOpen || board[row][col].isFlagged) {
+		if (board[row][col].isOpen || board[row][col].isFlagged || gameOver || win) {
 			return;
 		}
 
@@ -75,7 +84,7 @@ const Minesweeper = () => {
 		if (!board[row][col].isMine && board[row][col].neighborMines === 0) {
 			const neighbors = [
 				[-1, -1], [-1, 0], [-1, 1],
-				[0, -1],           [0, 1],
+				[0, -1], [0, 1],
 				[1, -1], [1, 0], [1, 1],
 			];
 
@@ -85,16 +94,47 @@ const Minesweeper = () => {
 
 				if (
 					newRow >= 0 &&
-					newRow < rows &&
+					newRow < width &&
 					newCol >= 0 &&
-					newCol < cols
+					newCol < height
 				) {
 					revealCell(newRow, newCol);
 				}
 			});
 		}
+
+		if (board[row][col].isMine) {
+			setGameOver(true);
+			Alert.alert('Вы проиграли', 'Вы попали на мину!');
+		}
 	}
 
+	function handleFlagToggle(row, col) {
+		if (!board[row][col].isOpen && !gameOver && !win) {
+			const updatedBoard = [...board];
+			updatedBoard[row][col].isFlagged = !board[row][col].isFlagged;
+			setBoard(updatedBoard);
+		}
+	}
+
+	function checkWin() {
+		const totalCells = width * height;
+		let revealedCells = 0;
+
+		for (let row = 0; row < width; row++) {
+			for (let col = 0; col < height; col++) {
+				if (board[row][col].isOpen) {
+					revealedCells++;
+				}
+			}
+		}
+
+		if (revealedCells === totalCells - minesCount) {
+			setWin(true);
+			setGameOver(true);
+			Alert.alert('Поздравляем', 'Вы выиграли!');
+		}
+	}
 
 	function renderBoard() {
 		return (
@@ -108,13 +148,20 @@ const Minesweeper = () => {
 									styles.cell,
 									cell.isOpen && styles.openCell,
 									cell.isMine && cell.isOpen && styles.mineCell,
+									cell.isFlagged && !cell.isOpen && styles.flaggedCell,
 								]}
-								onPress={() => revealCell(rowIndex, colIndex)}
+								onPress={() => {
+									if (flagMode) {
+										handleFlagToggle(rowIndex, colIndex);
+									} else {
+										revealCell(rowIndex, colIndex);
+									}
+								}}
 							>
 								{cell.isOpen && !cell.isMine && cell.neighborMines > 0 && (
 									<Text style={styles.mineCount}>{cell.neighborMines}</Text>
 								)}
-								{cell.isFlagged && <Text style={styles.flag}>🚩</Text>}
+								{cell.isFlagged && !cell.isOpen && <Text style={styles.flag}>🚩</Text>}
 								{cell.isOpen && cell.isMine && <Text style={styles.mine}>💣</Text>}
 							</TouchableOpacity>
 						))}
@@ -124,47 +171,84 @@ const Minesweeper = () => {
 		);
 	}
 
-
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Minesweeper</Text>
 			{renderBoard()}
-			<Button title="New Game" onPress={() => setBoard(initializeBoard())} />
+			<Button title={`Flag Mode: ${flagMode ? 'ON' : 'OFF'}`} onPress={toggleFlagMode} />
+			<Button title="New Game" onPress={() => resetGame()} />
 		</View>
 	);
+
+	function resetGame() {
+		setBoard(initializeBoard(width, height, minesCount));
+		setGameOver(false);
+		setWin(false);
+	}
 };
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 20,
+	},
 	board: {
 		flexDirection: 'column',
+		marginBottom: 20,
 	},
 	row: {
 		flexDirection: 'row',
 	},
 	cell: {
-		width: 30,
-		height: 30,
+		width: 36,
+		height: 36,
 		borderWidth: 1,
-		borderColor: '#ccc',
+		borderColor: '#ddd',
 		alignItems: 'center',
 		justifyContent: 'center',
+		backgroundColor: '#f0f0f0',
+		...Platform.select({
+			ios: {
+				shadowColor: 'rgba(0, 0, 0, 0.2)',
+				shadowOpacity: 0.5,
+				shadowRadius: 2,
+				shadowOffset: {
+					height: 2,
+					width: 0,
+				},
+			},
+			android: {
+				elevation: 3,
+			},
+		}),
 	},
 	openCell: {
-		backgroundColor: '#eee',
+		backgroundColor: '#fff',
 	},
 	mineCell: {
-		backgroundColor: 'red',
+		backgroundColor: '#dcdcdc',
 	},
 	mineCount: {
-		color: 'blue',
+		color: '#007bff',
+		fontSize: 16,
 	},
 	mine: {
 		color: 'red',
+		fontSize: 20,
 	},
 	flag: {
 		color: 'green',
+		fontSize: 16,
+	},
+	flaggedCell: {
+		backgroundColor: '#e1e1e1',
 	},
 });
-
 
 export default Minesweeper;
